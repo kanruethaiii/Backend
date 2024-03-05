af982cf19e4ef337aae68b5617adfa1354f148e0
gun <99672136+kanruethaiii@users.noreply.github.com>
2024-03-06 00:20:56 +0700

    ฟ

--- serverBack.js
+++ serverBack.js
@@ -401,4 +401,4 @@ app.delete('/categories/:id', async (req, res) => {
 /////////// categories /////////////
 
 
-app.listen(3000, () => { console.log(`Listening on port ${3000}`) })
+app.listen(3001, () => { console.log(`Listening on port ${3001}`) })
